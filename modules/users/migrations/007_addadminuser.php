8cd55979c5d65e33d1c8c048654f3c2c9e99f50e
Stefan Riedel <sr@alphabytes.de>
2013-01-30 08:32:58 +0100

    Admin User wird jetzt angelegt 

--- modules/users/migrations/007_addadminuser.php
+++ modules/users/migrations/007_addadminuser.php
@@ -5,25 +5,15 @@
  */
 namespace Fuel\Migrations;
 
-class Createprofiletable
+class Addadminuser
 {
     public function up()
     {
-        \DBUtil::create_table('profiles', array(
-            'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true),
-            'user_id' => array('constraint' => 11, 'type' => 'int'),
-            'firstname' => array('constraint' => 50, 'type' => 'varchar'),
-            'lastname' => array('constraint' => 50, 'type' => 'varchar'),
-            'birthday' => array('type' => 'date', 'null' => true),
-            'gender' => array('constraint' => 1, 'type' => 'varchar', 'null' => true),
-            'created_at' => array('type' => 'datetime'),
-            'updated_at' => array('type' => 'datetime'),
-
-        ), array('id', 'user_id'));
+        \Auth\Auth::create_user('admin', 'admin', 'admin@example.com', 100);
     }
 
     public function down()
     {
-        \DBUtil::drop_table('profiles');
+       \Auth\Auth::delete_user('admin');
     }
 }
