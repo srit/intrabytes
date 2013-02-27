1a432fc37560083be6d6f96898638c7ce7589c76
Stefan Riedel <sr@alphabytes.de>
2013-02-27 09:49:55 +0100

    Bootstrap Pagination aktiviert

---
+++ fuel/packages/srit/config/pagination.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @created 27.02.13 - 09:45
+ * @author stefanriedel
+ */
+
+return array(
+    'active' => 'bootstrap',
+
+    'bootstrap' => array(
+        'wrapper' => "<div class=\"pagination pagination-small\">\n\t<ul>{pagination}\n\t</ul>\n</div>\n",
+    )
+);
