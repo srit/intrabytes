6380dce60ad7daef69c629e92a7cba836f4ff48d
Stefan Riedel <sr@alphabytes.de>
2013-03-18 07:16:12 +0100

    CRUD routings added

--- modules/users/config/routes.php
+++ modules/users/config/routes.php
@@ -5,5 +5,11 @@
  */
 
 return array(
-    'users/password/confirmed_email/:hash' => 'users/password/confirmed_email'
+    'users/password/confirmed_email/:hash' => 'users/password/confirmed_email',
+
+    'users/settings/pubkeys/list' => array('users/settings/pubkeys/list', 'name' => 'users_settings_pubkeys_list'),
+    'users/settings/pubkeys/add' => array('users/settings/pubkeys/add', 'name' => 'users_settings_pubkeys_add'),
+    'users/settings/pubkeys/edit/(:id)' => array('users/settings/pubkeys/edit', 'name' => 'users_settings_pubkeys_edit'),
+    'users/settings/pubkeys/delete/(:id)' => array('users/settings/pubkeys/delete', 'name' => 'users_settings_pubkeys_delete'),
+    'users/settings/pubkeys/show/(:id)' => array('users/settings/pubkeys/show', 'name' => 'users_settings_pubkeys_show'),
 );
