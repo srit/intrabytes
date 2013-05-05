952c37e229285ff33c46f27cd4c0f18e568b6f3d
Stefan Riedel <sr@alphabytes.de>
2013-05-05 20:45:01 +0200

    typeahead with new right paths typeahead with new right field ids

--- public/themes/default/js/modules/core/main.js
+++ public/themes/default/js/modules/core/main.js
@@ -18,9 +18,23 @@ $(document).ready(function () {
     $('#postalcode_text[data-provide="typeahead"]').typeahead({
         source: function (query, process) {
             var link = $(this)[0].$element[0].dataset.link;
-            console.log(language_id);
-            return $.getJSON(link, { query: query, what: what, language_id: language_id }, function (data) {
+            var country_id = $('#country_id option:selected').val();
+            var what = $(this)[0].$element[0].name;
+            return $.getJSON(link, { query: query, country_id: country_id, what: what }, function (data) {
                 return process(data.options);
+            });
+        },
+        updater: function (item, process) {
+            var map = item.split(" - ");
+            var link = '/core/customers/postalcodes/rest/fetch' + uri_suffix;
+            var country_id = $('#country_id option:selected').val();
+            $.getJSON(link, { postalcode: map[0], country_id: country_id }, function (data) {
+                var postalcode_id_field = $('#postalcode_id');
+                var postalcode_text_field = $('#postalcode_text');
+                var city_text_field = $('#city_text');
+                postalcode_id_field.val(data.id);
+                city_text_field.val(data.city);
+                postalcode_text_field.val(data.postalcode);
             });
         }
     });
